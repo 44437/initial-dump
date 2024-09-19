0cf92c01410930fe886680ddf774b3539db6cf92
44437 <ercantopuz33@gmail.com>
2024-09-19 19:38:02 +0300

    Initial commit

---
+++ src/main/java/com/u44437/initial_dump/InitialDumpApplication.java
@@ -0,0 +1,13 @@
+package com.u44437.initial_dump;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitialDumpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitialDumpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/u44437/initial_dump/InitialDumpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.u44437.initial_dump;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitialDumpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
