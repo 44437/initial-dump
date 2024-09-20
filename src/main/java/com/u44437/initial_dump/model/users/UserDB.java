2313de8991d460115c9a2b15cec512b9c8c95449
44437 <ercantopuz33@gmail.com>
2024-09-20 11:09:50 +0300

    Fix the problem in the annotation @Data when compiling

--- src/main/java/com/u44437/initial_dump/model/users/UserDB.java
+++ src/main/java/com/u44437/initial_dump/model/users/UserDB.java
@@ -1,5 +1,8 @@
 package com.u44437.initial_dump.model.users;
 
+import lombok.Data;
+
+@Data
 public class UserDB {
   private int id;
   private String name;
@@ -8,30 +11,6 @@ public class UserDB {
   public UserDB(int id, String name, String surname) {
     this.id = id;
     this.name = name;
-    this.surname = surname;
-  }
-
-  public int getId() {
-    return id;
-  }
-
-  public void setId(int id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getSurname() {
-    return surname;
-  }
-
-  public void setSurname(String surname) {
     this.surname = surname;
   }
 }
