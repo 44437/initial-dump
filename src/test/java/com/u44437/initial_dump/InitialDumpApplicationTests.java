968d1647242ff908e8ec9b78e30c58f0243db896
44437 <ercantopuz33@gmail.com>
2024-09-25 15:59:37 +0300

    Add the tests of the main

--- src/test/java/com/u44437/initial_dump/InitialDumpApplicationTests.java
+++ src/test/java/com/u44437/initial_dump/InitialDumpApplicationTests.java
@@ -1,13 +1,10 @@
 package com.u44437.initial_dump;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class InitialDumpApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void Main() {
+    InitialDumpApplication.main(new String[] {});
+  }
 }
